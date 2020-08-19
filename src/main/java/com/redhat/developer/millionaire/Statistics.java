7d962dda0d582b6b45e955a6ec1f39d6de099d5d
sebastienblanc <scm.blanc@gmail.com>
2020-08-19 14:32:25 +0200

    overlay fixes

--- src/main/java/com/redhat/developer/millionaire/Statistics.java
+++ src/main/java/com/redhat/developer/millionaire/Statistics.java
@@ -17,23 +17,23 @@ public class Statistics {
 
     private Map<String, QuestionCounter> questions = new HashMap<>();
 
-    @Lock
+    
     public void reset() {
         this.numberOfRegisteredUsers = 0;
         this.questions.clear();
     }
 
-    @Lock
+   
     public void incrNumberOfUsers() {
         numberOfRegisteredUsers++;
     }
 
-    @Lock
+  
     public long getNumberOfRegisteredUsers() {
         return numberOfRegisteredUsers;
     }
 
-    @Lock
+    
     public long getTotalAnsweredQuestions() {
         return questions.values()
                     .stream()
@@ -41,18 +41,18 @@ public class Statistics {
                     .sum();
     }
 
-    @Lock
+   
     public void incr(String questionId, String answerId) {
         questions.computeIfAbsent(questionId, k -> new QuestionCounter())
                  .incr(answerId);
     }
 
-    @Lock
+    
     public Optional<QuestionCounter> getQuestionCounter(String questionId) {
         return Optional.ofNullable(questions.get(questionId));
     }
 
-    @Lock
+    
     public Map<String, QuestionCounter> getQuestions() {
         return questions;
     }
