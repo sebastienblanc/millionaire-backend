5d297bb68400d6caf6f7b9b8f252452f7aaba155
sebastienblanc <scm.blanc@gmail.com>
2020-08-21 17:18:54 +0200

    other fixes

--- src/main/java/com/redhat/developer/millionaire/IdentifierGenerator.java
+++ src/main/java/com/redhat/developer/millionaire/IdentifierGenerator.java
@@ -14,7 +14,8 @@ public class IdentifierGenerator {
             MessageDigest md = MessageDigest.getInstance("SHA-1");
             md.update(from.getBytes());
             byte[] id = md.digest();
-            return Base64.getEncoder().encodeToString(id);
+            String id = Base64.getEncoder().encodeToString(id);
+            return id;
         } catch (NoSuchAlgorithmException e) {
             throw new IllegalStateException(e);
         }
